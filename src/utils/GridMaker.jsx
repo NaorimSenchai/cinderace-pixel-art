bc3362709201906f5221438aee28672d7e7cffc0
NaorimSenchai <romainchenais35@gmail.com>
2023-03-01 16:30:02 +0100

    Setup GitHub Pages

--- src/utils/GridMaker.jsx
+++ src/utils/GridMaker.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * Creates a grid based on the specified parameters
+ * @param {Object} params - The grid parameters
+ * @param {number} params.height - The height of the grid
+ * @param {number} params.width - The width of the grid
+ * @param {function} [params.clickedCell] - The callback function for clicking on a cell
+ * @returns {JSX.Element} - The JSX element representing the grid
+ */
 const GridMaker = (params) => {
   let tmp = [];
 
